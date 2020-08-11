bd63a1e4bc1d9d46c5b7e83ec7b84c9bc8026bcc
Matthias Niggli <matthias.niggli@unibas.ch>
2020-08-11 08:57:07 +0200

    init scripts

---
+++ Data_Creation/data_creation.R
@@ -0,0 +1,18 @@
+#########################################################
+# Use this file to create the data used for the         #
+# report on innovation origins.                         #
+# Authors:      Matthias Niggli/CIEB UniBasel           #
+#                 Christian Rutzer/CIEB UniBasel        #
+#                 Dragan Filimonovic/CIEB UniBasel      #
+# Date:           11.08.2020                            #
+#########################################################
+
+
+###########################################
+#### LOAD PACKAGES AND SET DIRECTORIES ####
+###########################################
+
+
+
+
+### --------------- PLATZHALTER FILE -----------------------------------
