16498e0ee7724b26876e8804d73e1d0510b64cbb
ch54321dircd ..dir22 <christian.rutzer@unibas.ch>
2020-08-17 18:22:17 +0200

    - some comments

--- Data_Creation/pat_geo_true.R
+++ Data_Creation/pat_geo_true.R
@@ -14,7 +14,16 @@ require(fuzzyjoin)
 require(tm)
 
 mainDir1 <- c("/scicore/home/weder/GROUP/Innovation/01_patent_data")
-tech_field_start <- 20
+
+## Initial parameters
+
+# Determine the tech_field manually 
+tech_field_start_index <- c(1, 35) ## calculation is done from first tech_field (input one) to last tech_field (input two) 
+
+# Determine the tech_field using slurm
+# args <- commandArgs(TRUE)
+# tech_field_start_index <- c(as.numeric(as.character(args[1])), as.numeric(as.character(args[2])))
+
 
 cross_bord_func <- function(tech_field_start, ctry_firm, inv_ctry){
 ## Load data created in 20 and 21: We only use USPTO patents, because for EPO patents we do not have longitudes or latitudes. So the results can only be seen as a representative sample. WE COULD DISCUSS AN EXTENSION TO EPO PATENTS 
@@ -105,7 +114,7 @@ return(inv_firm)
 }
 
 ## apply previous created function to different technology fields. The tech_fields are determined by the first input of the lapply function
-inv_firm_adj <- do.call(rbind.fill, lapply(seq(16, 16), function(x) cross_bord_func(x, "CH", c("DE", "FR", "IT", "AT", "CH")))) ##cross_bord_func: first input parameter is for the country where the patent has been invented and the second for the countries where the inventors may come from (crossborder-commuters and domestic residence). 
+inv_firm_adj <- do.call(rbind.fill, lapply(seq(tech_field_start_index[1], tech_field_start_index[2], 1), function(x) cross_bord_func(x, "CH", c("DE", "FR", "IT", "AT", "CH")))) ##cross_bord_func: first input parameter is for the country where the patent has been invented and the second for the countries where the inventors may come from (crossborder-commuters and domestic residence). 
 ## Description of variables: ctry_firm: Country of the firm as written in the patent; ctry_inv: country of the place of resindece of the inventor as written in the patent; ctry_pat: Our derived country where the innovation has happened
 
 ## Add p_year
